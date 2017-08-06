a623bcdb48ab0878d091d45dfbf07b4c1ae49ce5
Alpine Devcamp <alpine@devcamp.com>
2017-08-06 05:12:06 +0000

    Implementecd icons in breadcrumbs

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -19,6 +19,6 @@ class TopicsController < ApplicationController
   private
   
   def set_sidebar_topics
-    @side_bar_topics = Topics.with_blogs
+    @side_bar_topics = Topic.with_blogs
   end
 end
